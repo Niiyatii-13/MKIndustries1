a187cb2abf8f7dba4de4941260705c6f79b74a70
Niiyatii-13 <166405063+Niiyatii-13@users.noreply.github.com>
2024-10-15 12:10:04 +0530

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -27,7 +27,7 @@ const addFood = async (req, res) => {
     })
     try {
         await food.save();
-        res.json({ success: true, message: "Food Added" })
+        res.json({ success: true, message: "Product Added" })
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" })
@@ -42,7 +42,7 @@ const removeFood = async (req, res) => {
         fs.unlink(`uploads/${food.image}`, () => { })
 
         await foodModel.findByIdAndDelete(req.body.id)
-        res.json({ success: true, message: "Food Removed" })
+        res.json({ success: true, message: "Product Removed" })
 
     } catch (error) {
         console.log(error);
