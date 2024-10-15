7d5b3ba23f85d2609c229a6e8e6660c34776e0d3
Niiyatii-13 <166405063+Niiyatii-13@users.noreply.github.com>
2024-10-15 12:34:39 +0530

    Update Orders.jsx

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -64,7 +64,7 @@ const Order = () => {
             <p>Items : {order.items.length}</p>
             <p>${order.amount}</p>
             <select onChange={(e)=>statusHandler(e,order._id)} value={order.status} name="" id="">
-              <option value="Food Processing">Food Processing</option>
+              <option value="Processing">Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
             </select>
